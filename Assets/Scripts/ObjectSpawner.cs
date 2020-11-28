17a958452b68da1c7e8a90420376d8a0025ec0d4
LSJ <sneak7581@wku.ac.kr>
2020-11-28 15:27:24 +0900

    Object Spawner 스크립트 수정

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     public GameObject objectToSpawn; // 생성할 게임오브젝트
+    private GameObject obj; // 집어넣을 게임 오브젝트
+    private bool isExist = false; // 오브젝트 존재 판단
     private PlacementIndicator placementIndicator; // 평지 표시기
+
     void Start()
     {
         placementIndicator = FindObjectOfType<PlacementIndicator>();
 
+        if(placementIndicator != null) // 평면 감지 되었으면
+        {
+            objectToSpawn.SetActive(true); // 오브젝트 비주얼 활성화
+        }
     }
 
     void Update()
     {
         // 터치를 했나 ? && ???????????
-        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         {
-            GameObject obj = Instantiate(objectToSpawn, 
-            placementIndicator.transform.position, 
-            placementIndicator.transform.rotation);
+            if (!isExist) // obj가 존재하지 않을 때 생성
+            {
+                obj = Instantiate(objectToSpawn,
+                placementIndicator.transform.position,
+                placementIndicator.transform.rotation);
+                isExist = true;
+            }
+            else if(isExist) // 존재하면 위치만 바꿔줌
+            {
+                obj.transform.position = placementIndicator.transform.position;
+                obj.transform.rotation = placementIndicator.transform.rotation;
+            }
         }
     }
 }
