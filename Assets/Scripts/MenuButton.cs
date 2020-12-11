1854e58d878eefa164554299caba47440a7e96df
LSJ <sneak7581@wku.ac.kr>
2020-12-11 12:50:30 +0900

    panel Visual 오타 정정

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -9,18 +9,15 @@ public class MenuButton : MonoBehaviour
     // public TextMeshProUGUI textMeshPro; // 디버그용 TMP
     private ObjectSpawner objectToSpawn;
     private PlacementIndicator placementIndicator;
-    public GameObject paneVisual; // 패널 비주얼
+    public GameObject panelVisual; // 패널 비주얼
     public GameObject objectSpawner;
 
     [SerializeField]
     private GameObject checkedButton; // 확인 버튼
     public static bool isChecked = false;  // 확인
     public static int menuNum = 0;
-
-    // [SerializeField]
-    // private GameObject spawnObject; // 생성할 오브젝트( object spawner에 존재하는거 )
     public GameObject[] pre; // 프리팹
-    //public GameObject[] indicator; // 인디케이터( 메뉴판, 평지표시기 )
+
 
     private void Start()
     {
@@ -58,10 +55,10 @@ public class MenuButton : MonoBehaviour
             PlacementIndicator.isOnOff = false;
             if (!PlacementIndicator.isOnOff)
             {
-                paneVisual.GetComponent<PlacementIndicator>().visual[0].SetActive(false);
-                paneVisual.GetComponent<PlacementIndicator>().visual[1].SetActive(true);
+                panelVisual.GetComponent<PlacementIndicator>().visual[0].SetActive(false);
+                panelVisual.GetComponent<PlacementIndicator>().visual[1].SetActive(true);
                 objectToSpawn.enabled = true;
-                checkedButton.SetActive(false); //초기화 버튼 사라지게
+                checkedButton.SetActive(false); // 초기화 버튼 사라지게
                 objectSpawner.GetComponent<ObjectSpawner>().enabled = true;
             }
         }
@@ -69,39 +66,7 @@ public class MenuButton : MonoBehaviour
 
     public void ReturnMenuPanel(bool getEscapeBtn)
     {
-        paneVisual.GetComponent<PlacementIndicator>().visual[0].SetActive(true);
-        paneVisual.GetComponent<PlacementIndicator>().visual[1].SetActive(false);
+        panelVisual.GetComponent<PlacementIndicator>().visual[0].SetActive(true);
+        panelVisual.GetComponent<PlacementIndicator>().visual[1].SetActive(false);
     }
-    
-    // public void ClickMenu()
-    // {
-    //     if (objectToSpawn.name == "Menu1")
-    //     {
-
-    //         isChecked_Num = 1;
-    //         if (isChecked_Num != 1)
-    //         {
-    //             checkedButton.SetActive(false);
-    //             isChecked_Num = 0;
-    //         }
-    //         else if (isChecked_Num == 1)
-    //         {
-    //             checkedButton.SetActive(true);
-
-    //         }
-
-    //     }
-    //     if (objectToSpawn.name == "Ham" && isChecked_Num != 0)
-    //     {
-    //         isChecked_Num = 2;
-    //         if (isChecked_Num != 2)
-    //         {
-    //             checkedButton.SetActive(false);
-    //         }
-    //         else if (isChecked_Num == 2)
-    //         {
-    //             checkedButton.SetActive(true);
-    //         }
-    //     }
-    // }
 }
