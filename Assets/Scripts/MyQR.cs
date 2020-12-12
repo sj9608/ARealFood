8bfa6c9df901d0baca1a9972c49c03d8e87c0c14
LSJ <sneak7581@wku.ac.kr>
2020-12-13 00:57:34 +0900

    qr을 인식 했을 때 메세지를 띄운다. (지정된것 안된것 포함.) 타이머 추가

--- Assets/Scripts/MyQR.cs
+++ Assets/Scripts/MyQR.cs
@@ -1,8 +1,9 @@
 ﻿using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 using ZXing;
+using DG.Tweening;
 
 public class MyQR : MonoBehaviour
 {
@@ -13,11 +14,11 @@ public class MyQR : MonoBehaviour
     private Rect rect; // 인식하는 사이즈
     private int W, H; // 스크린 사이즈
     private bool isQuit; // 어플 종료관련
-
-    // public Text txt; // 바꿔줄 텍스트 디버그 용도
-
+    private bool isQrDetected = false; // Qr이 감지 되었는가 ?
+    private float timer; // 타이머
+    private float waitingTime = 2.0f; // 대기 시간
+    public TextMeshProUGUI qrMessage; // TMP QR 메세지  (화면 중앙 하단부)
     public string LastResult; // Decode된 텍스트가 들어갈 변수
-    public static bool shouldDecodeNow; // QR코드 디코드 해야하는 상황판단
 
     void OnDestroy()
     {
@@ -35,34 +36,59 @@ public class MyQR : MonoBehaviour
         H = Screen.height;
 
         rect = new Rect(0, 0, W, H);
-        qrTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-
-
+        qrTexture = new Texture2D(W, H, TextureFormat.RGB24, false);
+        
         LastResult = "";
-        shouldDecodeNow = true;
 
         qrThread = new Thread(DecodeQR);
         qrThread.Start();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (shouldDecodeNow)
+        /* QR 인식 */
+        qrTexture.ReadPixels(rect, 0, 0, false);
+        qrTexture.Apply();
+
+        /* QR 감지 */
+        DetectQR(); 
+    }
+
+    private void DetectQR()
+    {
+        if (isQrDetected) // QR이 감지 되었고
         {
-            qrTexture.ReadPixels(rect, 0, 0, false);
-            qrTexture.Apply();
-        }
-        if(LastResult == "FirstFood")
-        {
-            menuPanel.SetActive(true);
-            DeActive();
+            if (LastResult == "FirstFood") // 디코드한 결과 text가 지정해놓은 text일 경우
+            {
+                // 텍스트 값 변경 
+                qrMessage.text = "인식 완료";
+                // 딜레이 코드 넣기 
+                timer += Time.deltaTime;
+                if (timer > waitingTime)
+                {
+                    menuPanel.SetActive(true);
+                    DeActive(); // QR 패널 비활성화
+                }
+            }
+            else
+            {
+                qrMessage.text = "등록되지 않은 코드입니다.";
+                // 딜레이 코드 넣기 (2초)
+                timer += Time.deltaTime;
+                if (timer > waitingTime) // 2초 뒤엔 다시 QR메세지를 원상태로 돌려준다.
+                {
+                    qrMessage.text = "QR코드를 중앙에 위치 시키십시오.";
+                    timer = 0;
+                    isQrDetected = false; // Qr감지가 안된 상태로 돌린다.
+                }
+
+            }
         }
     }
 
-    void DecodeQR()
+    void DecodeQR() // Qr코드를 디코드
     {
-        var barcodeReader = new BarcodeReader();
+        var barcodeReader = new BarcodeReader(); // ZXing 라이브러리의 BarcodeReader 형 변수 barcodeReader 
 
         while (true)
         {
@@ -71,16 +97,16 @@ public class MyQR : MonoBehaviour
             try
             {
                 // decode the current frame
-                var result = barcodeReader.Decode(qrTexture.GetPixels32(), W, H);
+                var result = barcodeReader.Decode(qrTexture.GetPixels32(), W, H); // ZXing 라이브러리의 Result형 변수 result에 인식된 범위의 텍스쳐의 픽셀을 읽고 해독
 
-                if (result != null)
+                if (result != null) // 결과 값이 도출이 되면 (Qr인식에 성공하면)
                 {
-                    LastResult = result.Text;
-                    shouldDecodeNow = false;
+                    LastResult = result.Text; // 결과값의 텍스트를 넘겨줌.
+                    isQrDetected = true; // Qr감지판단을 참으로 함. (qr 인식 됌)
                 }
 
                 // Sleep a little bit and set the signal to get the next frame
-                Thread.Sleep(200);
+                Thread.Sleep(200); // 스레드를 재운다. 0.2초 뒤
             }
             catch
             {
@@ -90,6 +116,6 @@ public class MyQR : MonoBehaviour
 
     void DeActive()
     {
-        this.gameObject.SetActive(false);
+        this.gameObject.SetActive(false); // 현재 스크립트가 붙은 오브젝트 즉 qrPanel 비활성화
     }
 }
